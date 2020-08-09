daa59026bf3af3e5f520c4f2bcffe7374150d28e
cem <a2fstart@gmail.com>
2020-08-09 14:41:15 +0300

    finished cookbook images section

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final title = 'Cached Images';
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      title: title,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter imageholder demo'),
+          title: Text(title),
         ),
         body: Center(
-          child: Column(
-            children: [
-              Text('Static image'),
-              Image.network(
-                'https://picsum.photos/250?image=9',
-              ),
-              Padding(padding: EdgeInsets.all(10.0)),
-              Text('GIF'),
-              Image.network(
-                'https://github.com/flutter/plugins/raw/master/packages/video_player/video_player/doc/demo_ipod.gif?raw=true',
-              ),
-            ],
+          child: CachedNetworkImage(
+            placeholder: (context, url) => CircularProgressIndicator(),
+            imageUrl: 'https://picsum.photos/250?image=9',
           ),
         ),
       ),
